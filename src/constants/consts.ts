9382dfb03df112419a61f3bb021a66d5064cb407
IsolatedLun <herojoe210@gmail.com>
2021-12-15 09:24:10 +0200

    final

--- src/constants/consts.ts
+++ src/constants/consts.ts
@@ -1,7 +1,8 @@
 const ROOT_URL = 'https://pzwiki.net/';
+const BACKEND_URL = 'https://pz-compare.herokuapp.com/'
 const STATS = ['MaxRange', 'MaxDamage', 'WeaponLength', 'DoorDamage', 
 'TreeDamage','Weight', 'MinRange', 'SwingTime', 'BaseSpeed', 'MinDamage', 'MaxHitCount', 
 'MinAngle','CriticalChange', 'ConditionMax', 'JamGunChance', 'StopPower', 'ReloadTime', 
 'AimingTime','MaxAmmo', 'SoundRadius', 'RecoilDelay', 'ProjectileCount', 'Calories']
 
-export { ROOT_URL, STATS };
+export { ROOT_URL, STATS, BACKEND_URL };
--- src/utils/objApi.ts
+++ src/utils/objApi.ts
@@ -1,7 +1,8 @@
 import axios from "axios"
 import { parseName } from "./funcs"
+import { BACKEND_URL } from "../constants/consts"
 
 export const getObjs = (objA: string, objB: string, filter: boolean) => {
-    return axios.get('http://localhost:8000/pz/' + parseName(objA) + '/with/' 
+    return axios.get(BACKEND_URL + 'pz/' + parseName(objA) + '/with/' 
         + parseName(objB) + '?filter=' + filter)
 }
